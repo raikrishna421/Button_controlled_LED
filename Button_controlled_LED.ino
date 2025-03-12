4a6d47a5da34cdfd4aca01879278d86d3eef5a88
raikrishna421 <80667120+raikrishna421@users.noreply.github.com>
2025-03-12 21:31:39 +0530

    Initial commit

---
+++ Button_controlled_LED.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(7,INPUT);
+pinMode(8,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int button=digitalRead(7);
+Serial.println(button);
+if(button==HIGH)
+{
+  digitalWrite(8,HIGH);
+  
+}
+else{
+  digitalWrite(8,LOW);
+}
+}
